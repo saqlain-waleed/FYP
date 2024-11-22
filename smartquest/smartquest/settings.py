35aeeea819b3a0e38ca4301e86ea60903a8f8783
saqlain waleed <saqlainwaleed18@gmail.com>
2024-11-22 18:50:20 +0500

    accounts app

--- smartquest/smartquest/settings.py
+++ smartquest/smartquest/settings.py
@@ -38,9 +38,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'comform.apps.ComformConfig',
+    'accounts.apps.AccountsConfig',
     'rest_framework',
     'corsheaders',
+    'djoser',
 ]
+
+
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
@@ -88,6 +92,15 @@ DATABASES = {
     }
 }
 CORS_ORIGIN_WHITELIST=['http://localhost:5173']
+
+
+
+EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST='smtp.gmail.com'
+EMAIL_PORT=587
+EMAIL_HOST_USER='waleedsaqlain1@gmail.com'
+EMAIL_HOST_PASSWORD='cmcwndyailfwhgka'
+EMAIL_USE_TLS=True
 
 
 # Password validation
@@ -138,3 +151,34 @@ import os
 MEDIA_ROOT=os.path.join(BASE_DIR, 'media')
 
 MEDIA_URL='/media/'
+
+
+AUTH_USER_MODEL='accounts.UserAccount'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
+SIMPLE_JWT = {
+   'AUTH_HEADER_TYPES': ('JWT',),
+}
+DJOSER={
+    'LOGIN_FIELD':'email',
+    'USER_CREATE_PASSWORD_RETYPE':True,
+    'USERNAME_CHANGED_EMAIL_CONFIRMATION':True,
+    'PASSWORD_CHANGED_EMAIL_CONFIRMATION':True,
+    'SEND_CONFIRMATION_EMAIL':True,
+    'SET_PASSWORD_RETYPE':True,
+    'PASSWORD_RESET_CONFIRM_URL':'password/reset/confirm/{uid}/{token}',
+    'USERNAME_RESET_CONFIRM_URL':'email/reset/confirm/{uid}/{token}',
+    'ACTIVATION_URL':'activate/{uid}/{token}',
+    'SEND_ACTIVATION_EMAIL':True,
+    'SERIALIZERS':{
+        'user_create':'accounts.serializers.UserCreateSerializer',
+        'user':'accounts.serializers.UserCreateSerializer',
+        'user_delete':'djoser.serializers.UserDeleteSerializer',
+
+    }
+}
--- smartquest/smartquest/urls.py
+++ smartquest/smartquest/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include ,re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView 
 
 
 
@@ -13,5 +14,9 @@ route.register("",comformview, basename='comformview')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('comform/',include(route.urls))
+    path('comform/',include(route.urls)),
+    path('auth/',include('djoser.urls')),
+    path('auth/',include('djoser.urls.jwt')),
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+
+urlpatterns += [re_path('^.*', TemplateView.as_view(template_name='index.html'))]
